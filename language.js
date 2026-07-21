b964c6ff706465f3f81e55d748bf9a70cd5f6628
igor-skudar <igorskudar@outlook.com>
2026-07-22 00:09:53 +0400

    Fix automatic language detection and user language preference handling

--- language.js
+++ language.js
@@ -1,6 +1,13 @@
 const savedLanguage = localStorage.getItem("language");
 
-if (!savedLanguage) {
+if (savedLanguage) {
+  if (savedLanguage === "ru" && !window.location.pathname.endsWith("ru.html")) {
+    window.location.href = "ru.html";
+  } 
+  if (savedLanguage !== "ru" && window.location.pathname.endsWith("ru.html")) {
+    window.location.href = "index.html";
+  }
+} else {
   const browserLanguage = navigator.language.toLowerCase();
 
   if (browserLanguage.startsWith("ru") && !window.location.pathname.endsWith("ru.html")) {
